ed2da1827ce53a2c8d828764a933540a4d559726
Anna Cheng <101126737+AnnAapompom@users.noreply.github.com>
2022-06-02 13:52:05 -0400

    Updated TitleScreenWorld

--- TitleScreenWorld.java
+++ TitleScreenWorld.java
@@ -17,5 +17,14 @@ public class TitleScreenWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 400, 1); 
+        
+        Label title = new Label("Insert Title Here", 70);
+        addObject(title, 350, 200);
+        //add start button 
+    }
+    
+    public void act()
+    {
+        // if mouse clicked if(Greenfoot.)  
     }
 }
