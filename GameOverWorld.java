7945280d2eac26b8a308634974e6b1132a09bc98
Anna Cheng <101126737+AnnAapompom@users.noreply.github.com>
2022-06-02 13:42:49 -0400

    Added empty classes and subclasses; imported Label and SimpleTimer class

--- GameOverWorld.java
+++ GameOverWorld.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Levels here.
+ * Write a description of class GameOverWorld here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Levels extends World
+public class GameOverWorld extends World
 {
 
     /**
-     * Constructor for objects of class Levels.
+     * Constructor for objects of class GameOverWorld.
      * 
      */
-    public Levels()
+    public GameOverWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 400, 1); 
--- InstructionsWorld.java
+++ InstructionsWorld.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Levels here.
+ * Write a description of class InstructionsWorld here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Levels extends World
+public class InstructionsWorld extends World
 {
 
     /**
-     * Constructor for objects of class Levels.
+     * Constructor for objects of class InstructionsWorld.
      * 
      */
-    public Levels()
+    public InstructionsWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 400, 1); 
