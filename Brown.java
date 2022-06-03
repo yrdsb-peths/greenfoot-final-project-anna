c3d367289cbb3636197e29c04194ea508b4890e8
Anna Cheng <101126737+AnnAapompom@users.noreply.github.com>
2022-06-03 10:41:00 -0400

    Added all background subclasses

---
+++ Brown.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Brown here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Brown extends Backgrounds
+{
+    /**
+     * Act - do whatever the Brown wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
---
+++ GreenBlocks.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GreenBlocks here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GreenBlocks extends Backgrounds
+{
+    /**
+     * Act - do whatever the GreenBlocks wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
